529249a363accb42299b3c4ec5e95ef8cc2d3bd7
Carsten Schneider <carsten.schneider@shopgate.com>
2020-06-26 14:06:53 +0200

    CCP-2202 added support for UM

--- frontend/subscriptions.js
+++ frontend/subscriptions.js
@@ -6,11 +6,41 @@ import { sdkUrl, pagesWithoutWidget } from './config';
 import { getUserData } from './selectors';
 
 export default (subscribe) => {
+  const { style } = document.documentElement;
   let ready = false;
 
-  css.global('#userlike-tab', {
+  // "live chat" -> userlike-tab. "UM" -> #uslk-button
+  css.global('#userlike-tab, #uslk-button', {
     bottom: 'calc(10px + var(--safe-area-inset-bottom) + var(--footer-height) ) !important',
   });
+  css.global('#userlikeButtonContainer', {
+    display: 'var(--userlike-um-display) !important',
+  });
+
+  // Flag to indicate if "live chat" or "unified messaging" is used
+  let isUMWidget = false;
+
+  /**
+   *  Hides the userlike button
+   */
+  const hideWidget = () => {
+    if (isUMWidget) {
+      style.setProperty('--userlike-um-display', 'none');
+    } else {
+      window.userlike.userlikeHideButton();
+    }
+  };
+
+  /**
+   *  Shows the userlike button again
+   */
+  const showWidget = () => {
+    if (isUMWidget) {
+      style.setProperty('--userlike-um-display', 'block');
+    } else {
+      window.userlike.userlikeShowButton();
+    }
+  };
 
   subscribe(appDidStart$, ({ getState }) => {
     if (!sdkUrl) {
@@ -31,8 +61,11 @@ export default (subscribe) => {
       const state = getState();
       const pattern = makeGetRoutePattern()(state);
 
+      // new UM does not have an getData function (yet?)
+      isUMWidget = typeof window.userlike.getData !== 'function';
+
       if (pagesWithoutWidget.includes(pattern)) {
-        window.userlike.userlikeHideButton();
+        hideWidget();
       }
 
       const user = getUserData(state);
@@ -66,24 +99,18 @@ export default (subscribe) => {
     window.userlike.userlikeUpdateAPI();
   });
 
-  subscribe(routeDidEnter$, ({ action }) => {
-    if (!window.userlike) {
-      return;
-    }
+  if (pagesWithoutWidget && pagesWithoutWidget.length) {
+    subscribe(routeDidEnter$, ({ action }) => {
+      if (!window.userlike) {
+        return;
+      }
 
-    if (pagesWithoutWidget.includes(action.route.pattern)) {
-      window.userlike.userlikeHideButton();
-    }
-  });
-
-  subscribe(routeDidEnter$, ({ action }) => {
-    if (!window.userlike) {
-      return;
-    }
-
-    if (!pagesWithoutWidget.includes(action.route.pattern)) {
-      window.userlike.userlikeShowButton();
-    }
-  });
+      if (pagesWithoutWidget.includes(action.route.pattern)) {
+        hideWidget();
+      } else {
+        showWidget();
+      }
+    });
+  }
 };
 
